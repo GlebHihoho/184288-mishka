b6b16966a040075ff616b0fd532a35a01da73c93
Gleb Motuz <hihoho@mail.ru>
2016-10-12 01:02:44 +0300

    Начал работу над стайл гайдом

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,13 +1,14 @@
 var mainMenuBtn = document.querySelector('.header__main-menu-btn');
-var mainMenu = document.querySelector('.main-menu');
+var mainMenu = document.querySelectorAll('.header__menu');
 var menuImg = document.querySelector('.header__btn-img');
 
 mainMenuBtn.addEventListener('click', openClose);
 
 function openClose(click) {
-  mainMenu.classList.toggle('open-close-js');
+  mainMenu[0].classList.toggle('open-close-js');
+  mainMenu[1].classList.toggle('open-close-js');
 
-  if (mainMenu.classList.contains('open-close-js')) {
+  if (mainMenu[0].classList.contains('open-close-js')) {
     menuImg.src = 'img/icon-menu-open.svg';
   } else {
     menuImg.src = 'img/icon-menu-close.svg';
